d78fb374d20af100920df7def7249639d2d1bdaa
Prof. Dr. Isaac G Andy, Ph.D <isaac@iznd.xyz>
2023-09-05 07:49:11 +0800

    Add files via upload

---
+++ scan_smartcontract.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import web3
+
+def scan_for_smart_contracts(wallet_address):
+    """Scans the Polygon blockchain for smart contracts that have been authorized to access the specified wallet.
+
+    Args:
+        wallet_address (str): The address of the wallet to scan.
+
+    Returns:
+        list: A list of smart contract addresses that have been authorized to access the specified wallet.
+    """
+
+    web3_provider = web3.Web3(Web3.HTTPProvider("https://polygon-rpc.com"))
+    account = web3_provider.eth.account(wallet_address)
+
+    url = "https://api.polygonscan.com/api/v1/contracts/list?address=" + wallet_address
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = json.loads(response.content)
+        smart_contract_addresses = []
+        for contract in data["contracts"]:
+            smart_contract_addresses.append(contract["address"])
+        return smart_contract_addresses
+    else:
+        raise Exception("Failed to connect to Polygonscan.")
+
+if __name__ == "__main__":
+    wallet_address = "0x1234567890abcdef01234567890abcdef0123456"
+    smart_contract_addresses = scan_for_smart_contracts(wallet_address)
+
+    for smart_contract_address in smart_contract_addresses:
+        # Trigger authorization of the wallet for the smart contract
+        web3_provider.eth.request_authentication(account, smart_contract_address)
